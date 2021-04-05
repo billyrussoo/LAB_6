eed8f673c5a1633aed4c8b7ca4a8b08bbda895d6
billyrussoo <80235793+billyrussoo@users.noreply.github.com>
2021-04-05 23:14:54 +0500

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Assignment {
+	 public static void main(String args[]){
+	       System.out.println("Sum "+ Calculator.sum(10,20));
+        System.out.println("Product "+ Calculator.multiply(10,20));
+        System.out.println("Division "+ Calculator.divide(10,20));
+        System.out.println("Remainder "+ Calculator.modulus(10,20));
+        System.out.println("Sine "+ Calculator.sin(4));
+        System.out.println("cos "+ Calculator.cos(3));
+        System.out.println("tan "+ Calculator.tan(4));
+
+	    }
+}
